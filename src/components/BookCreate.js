f08b077b865ea8510a2e74c55182fc4a3d518131
tsheporamantso <tshepo.ramantso@outlook.com>
2024-03-28 16:34:01 +0200

    Add form and functions to handle form submission and change

--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -1,7 +1,28 @@
+import { useState } from 'react'
 
+const BookCreate = ({onCreate}) => {
+  const [title, setTitle] = useState('') 
+  
+  const handleChange = (e) => {
+    setTitle(e.target.value)
+  };
 
-export default function BookCreate (){
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    onCreate(title)
+    setTitle('')
+
+  }
   return (
-    <div>BookCreate</div>
+    <div className="book-create">
+      <h3>Add a Book</h3>
+      <form onSubmit={handleSubmit}>
+        <label>Title</label> <br />
+        <input className="input" value={title} onChange={handleChange}/><br />
+        <button className="button" type="submit">Create</button>
+      </form>
+    </div>
   )
-}
+} 
+
+export default BookCreate
