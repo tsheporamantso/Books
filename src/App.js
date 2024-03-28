5a95cbda1d8b6b811f7e5f7b73993bb0d60562c9
tsheporamantso <tshepo.ramantso@outlook.com>
2024-03-28 16:32:09 +0200

    Add update book function and change state of books

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { useState } from 'react'
 import BookCreate from './components/BookCreate'
-import './index.css'
-
 
 const App = () => {
   const [books, setBooks] = useState([])
 
   const createBook = (title) => {
-    console.log('need to add book with title', title)
-    // setBooks(title.target.value)
+    const updateBooks = [...books, {title}]
+    console.log(updateBooks)
+    setBooks(updateBooks)
   }
   return (
     <div>
